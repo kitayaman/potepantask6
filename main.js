55ed403d0934c828ca5308ee54ad0ea493befa9c
EC2 Default User <ec2-user@ip-172-31-20-254.ap-northeast-1.compute.internal>
2021-05-04 08:32:13 +0000

    スリックスライダー完成

---
+++ main.js
@@ -0,0 +1,12 @@
+$('#slick_show').slick( {
+    autoplay:true,
+    autoplaySpeed:4000,
+    arrows:true,
+    prevArrow: '<div class="left-arrow">＜</div>',
+    nextArrow: '<div class="right-arrow">＞</div>',
+
+
+    
+    
+    
+});
