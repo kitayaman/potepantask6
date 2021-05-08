73c00287ef1067dc8ab02ba08188dcb980fa4243
EC2 Default User <ec2-user@ip-172-31-20-254.ap-northeast-1.compute.internal>
2021-05-08 07:55:51 +0000

    変更しました

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+$(function(){
 $('#slick_show').slick( {
-    autoplay:true,
-    autoplaySpeed:4000,
-    arrows:true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    infinit:true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
     prevArrow: '<div class="left-arrow">＜</div>',
-    nextArrow: '<div class="right-arrow">＞</div>',
-
-
-    
-    
+    nextArrow: '<div class="right-arrow">＞</div>'
+});
     
 });
